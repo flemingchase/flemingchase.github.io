c4cc94f5b0cab7b010a7663f95637dd118ab21cc
flemingchase <89117751+flemingchase@users.noreply.github.com>
2025-01-11 08:33:47 +0000

    Deploying to gh-pages from @ flemingchase/flemingchase.github.io@5f45b459b7a00442471b319af6938a3bf97afc37 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-blank",
+    title: "blank",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Courses taught and their syllabi",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
